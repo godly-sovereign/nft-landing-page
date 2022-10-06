993b657042d539c3e5a94fe2cfb531c0f397bc56
redtripleog <redragassasin@gmail.com>
2022-10-05 20:07:23 -0700

    changes and updates

---
+++ app.vue
@@ -0,0 +1,13 @@
+import { useRoute } from 'vue-router'
+import TheSidebar from './components/TheSidebar'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { initWallet } from 'solana-wallets-vue'
+
+const route = useRoute()
+
+const wallets = [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter(),
+]
+
+initWallet({ wallets, autoConnect: true })
---
+++ main.js
@@ -0,0 +1,5 @@
+// CSS.
+import 'solana-wallets-vue/styles.css'
+import './main.css'
+
+// ...
