95c5a98bd45807c3fa9c7e783dea8e3d73cfc3fe
redtripleog <redragassasin@gmail.com>
2022-10-29 11:04:18 -0700

    added game page and attach to min page

---
+++ commands.sh
@@ -0,0 +1,14 @@
+bash <(curl -sSf https://sugar.metaplex.com/install.sh)
+
+sh -c "$(curl -sSfL https://release.solana.com/v1.14.3/install)"
+
+sugar --version
+solana --version
+
+/Users/marquesstone/documents/github/nft-landing-page-1
+
+Dims3Ffm1epC6M9a92HFTar79pxLuarXTh7MuKm5oZLN
+
+oorzRamzZQzvfRQRaAVa5ehCuDEsRn7hWM5LP4Bhf4g.json
+
+sugar launch
