65b0a091d0adc9933e46e363ffd4cc1be27a41ab
redtripleog <redragassasin@gmail.com>
2022-11-28 17:50:52 -0800

    Adding game demo to website

---
+++ spgdemo/code2.js
@@ -0,0 +1,344 @@
+gdjs.Lvl2Code = {};
+gdjs.Lvl2Code.GDWesleyObjects1_1final = [];
+
+gdjs.Lvl2Code.GDStars_95ParticlesObjects1= [];
+gdjs.Lvl2Code.GDStars_95ParticlesObjects2= [];
+gdjs.Lvl2Code.GDRoomTrapsObjects1= [];
+gdjs.Lvl2Code.GDRoomTrapsObjects2= [];
+gdjs.Lvl2Code.GDRoomDoorsObjects1= [];
+gdjs.Lvl2Code.GDRoomDoorsObjects2= [];
+gdjs.Lvl2Code.GDRoomFloorObjects1= [];
+gdjs.Lvl2Code.GDRoomFloorObjects2= [];
+gdjs.Lvl2Code.GDRoomObjects1= [];
+gdjs.Lvl2Code.GDRoomObjects2= [];
+gdjs.Lvl2Code.GDWesleyObjects1= [];
+gdjs.Lvl2Code.GDWesleyObjects2= [];
+gdjs.Lvl2Code.GDGunObjects1= [];
+gdjs.Lvl2Code.GDGunObjects2= [];
+gdjs.Lvl2Code.GDImpObjects1= [];
+gdjs.Lvl2Code.GDImpObjects2= [];
+gdjs.Lvl2Code.GDSpiderObjects1= [];
+gdjs.Lvl2Code.GDSpiderObjects2= [];
+gdjs.Lvl2Code.GDGhostObjects1= [];
+gdjs.Lvl2Code.GDGhostObjects2= [];
+gdjs.Lvl2Code.GDGhostOrbObjects1= [];
+gdjs.Lvl2Code.GDGhostOrbObjects2= [];
+gdjs.Lvl2Code.GDBulletObjects1= [];
+gdjs.Lvl2Code.GDBulletObjects2= [];
+gdjs.Lvl2Code.GDUpgradeTextObjects1= [];
+gdjs.Lvl2Code.GDUpgradeTextObjects2= [];
+gdjs.Lvl2Code.GDEnemyDamageTextObjects1= [];
+gdjs.Lvl2Code.GDEnemyDamageTextObjects2= [];
+gdjs.Lvl2Code.GDParticle_95RecoilDustObjects1= [];
+gdjs.Lvl2Code.GDParticle_95RecoilDustObjects2= [];
+gdjs.Lvl2Code.GDParticle_95DashObjects1= [];
+gdjs.Lvl2Code.GDParticle_95DashObjects2= [];
+gdjs.Lvl2Code.GDParticle_95DeathObjects1= [];
+gdjs.Lvl2Code.GDParticle_95DeathObjects2= [];
+gdjs.Lvl2Code.GDHealthBarBorderObjects1= [];
+gdjs.Lvl2Code.GDHealthBarBorderObjects2= [];
+gdjs.Lvl2Code.GDHealthBarObjects1= [];
+gdjs.Lvl2Code.GDHealthBarObjects2= [];
+gdjs.Lvl2Code.GDDebugObjects1= [];
+gdjs.Lvl2Code.GDDebugObjects2= [];
+gdjs.Lvl2Code.GDUpgradeIconsObjects1= [];
+gdjs.Lvl2Code.GDUpgradeIconsObjects2= [];
+gdjs.Lvl2Code.GDUpgradesObjects1= [];
+gdjs.Lvl2Code.GDUpgradesObjects2= [];
+gdjs.Lvl2Code.GDTotalPointsCountObjects1= [];
+gdjs.Lvl2Code.GDTotalPointsCountObjects2= [];
+gdjs.Lvl2Code.GDTotalPointsObjects1= [];
+gdjs.Lvl2Code.GDTotalPointsObjects2= [];
+gdjs.Lvl2Code.GDDangerLevelCountObjects1= [];
+gdjs.Lvl2Code.GDDangerLevelCountObjects2= [];
+gdjs.Lvl2Code.GDDangerLevelObjects1= [];
+gdjs.Lvl2Code.GDDangerLevelObjects2= [];
+gdjs.Lvl2Code.GDHealthTextObjects1= [];
+gdjs.Lvl2Code.GDHealthTextObjects2= [];
+gdjs.Lvl2Code.GDPick_95UpsObjects1= [];
+gdjs.Lvl2Code.GDPick_95UpsObjects2= [];
+gdjs.Lvl2Code.GDReset_95TimerObjects1= [];
+gdjs.Lvl2Code.GDReset_95TimerObjects2= [];
+gdjs.Lvl2Code.GDLeaderboard_95SubmitObjects1= [];
+gdjs.Lvl2Code.GDLeaderboard_95SubmitObjects2= [];
+gdjs.Lvl2Code.GDReset_95LeaderboardObjects1= [];
+gdjs.Lvl2Code.GDReset_95LeaderboardObjects2= [];
+gdjs.Lvl2Code.GDReset_95ButtonObjects1= [];
+gdjs.Lvl2Code.GDReset_95ButtonObjects2= [];
+gdjs.Lvl2Code.GDDarkeningObjects1= [];
+gdjs.Lvl2Code.GDDarkeningObjects2= [];
+gdjs.Lvl2Code.GDLeaderboardName_95InputObjects1= [];
+gdjs.Lvl2Code.GDLeaderboardName_95InputObjects2= [];
+gdjs.Lvl2Code.GDPause_95TextObjects1= [];
+gdjs.Lvl2Code.GDPause_95TextObjects2= [];
+gdjs.Lvl2Code.GDUpgrade_95TextObjects1= [];
+gdjs.Lvl2Code.GDUpgrade_95TextObjects2= [];
+
+gdjs.Lvl2Code.conditionTrue_0 = {val:false};
+gdjs.Lvl2Code.condition0IsTrue_0 = {val:false};
+gdjs.Lvl2Code.condition1IsTrue_0 = {val:false};
+gdjs.Lvl2Code.condition2IsTrue_0 = {val:false};
+gdjs.Lvl2Code.condition3IsTrue_0 = {val:false};
+gdjs.Lvl2Code.condition4IsTrue_0 = {val:false};
+gdjs.Lvl2Code.condition5IsTrue_0 = {val:false};
+gdjs.Lvl2Code.condition6IsTrue_0 = {val:false};
+gdjs.Lvl2Code.conditionTrue_1 = {val:false};
+gdjs.Lvl2Code.condition0IsTrue_1 = {val:false};
+gdjs.Lvl2Code.condition1IsTrue_1 = {val:false};
+gdjs.Lvl2Code.condition2IsTrue_1 = {val:false};
+gdjs.Lvl2Code.condition3IsTrue_1 = {val:false};
+gdjs.Lvl2Code.condition4IsTrue_1 = {val:false};
+gdjs.Lvl2Code.condition5IsTrue_1 = {val:false};
+gdjs.Lvl2Code.condition6IsTrue_1 = {val:false};
+
+
+gdjs.Lvl2Code.mapOfGDgdjs_46Lvl2Code_46GDGhostObjects1ObjectsGDgdjs_46Lvl2Code_46GDSpiderObjects1ObjectsGDgdjs_46Lvl2Code_46GDImpObjects1Objects = Hashtable.newFrom({"Ghost": gdjs.Lvl2Code.GDGhostObjects1, "Spider": gdjs.Lvl2Code.GDSpiderObjects1, "Imp": gdjs.Lvl2Code.GDImpObjects1});
+gdjs.Lvl2Code.eventsList0 = function(runtimeScene) {
+
+{
+
+
+gdjs.Lvl2Code.condition0IsTrue_0.val = false;
+{
+{gdjs.Lvl2Code.conditionTrue_1 = gdjs.Lvl2Code.condition0IsTrue_0;
+gdjs.Lvl2Code.conditionTrue_1.val = runtimeScene.getOnceTriggers().triggerOnce(13342460);
+}
+}if (gdjs.Lvl2Code.condition0IsTrue_0.val) {
+gdjs.copyArray(runtimeScene.getObjects("DangerLevelCount"), gdjs.Lvl2Code.GDDangerLevelCountObjects1);
+{for(var i = 0, len = gdjs.Lvl2Code.GDDangerLevelCountObjects1.length ;i < len;++i) {
+    gdjs.Lvl2Code.GDDangerLevelCountObjects1[i].setString(gdjs.evtTools.common.toString(gdjs.evtTools.variable.getVariableNumber(runtimeScene.getVariables().getFromIndex(2))));
+}
+}}
+
+}
+
+
+{
+
+gdjs.copyArray(runtimeScene.getObjects("Ghost"), gdjs.Lvl2Code.GDGhostObjects1);
+gdjs.copyArray(runtimeScene.getObjects("Imp"), gdjs.Lvl2Code.GDImpObjects1);
+gdjs.copyArray(runtimeScene.getObjects("Spider"), gdjs.Lvl2Code.GDSpiderObjects1);
+gdjs.Lvl2Code.GDWesleyObjects1.length = 0;
+
+
+gdjs.Lvl2Code.condition0IsTrue_0.val = false;
+gdjs.Lvl2Code.condition1IsTrue_0.val = false;
+{
+gdjs.Lvl2Code.condition0IsTrue_0.val = gdjs.evtTools.object.pickedObjectsCount(gdjs.Lvl2Code.mapOfGDgdjs_46Lvl2Code_46GDGhostObjects1ObjectsGDgdjs_46Lvl2Code_46GDSpiderObjects1ObjectsGDgdjs_46Lvl2Code_46GDImpObjects1Objects) <= 0;
+}if ( gdjs.Lvl2Code.condition0IsTrue_0.val ) {
+{
+{gdjs.Lvl2Code.conditionTrue_1 = gdjs.Lvl2Code.condition1IsTrue_0;
+gdjs.Lvl2Code.GDWesleyObjects1_1final.length = 0;gdjs.Lvl2Code.condition0IsTrue_1.val = false;
+gdjs.Lvl2Code.condition1IsTrue_1.val = false;
+gdjs.Lvl2Code.condition2IsTrue_1.val = false;
+gdjs.Lvl2Code.condition3IsTrue_1.val = false;
+gdjs.Lvl2Code.condition4IsTrue_1.val = false;
+gdjs.Lvl2Code.condition5IsTrue_1.val = false;
+{
+gdjs.copyArray(runtimeScene.getObjects("Wesley"), gdjs.Lvl2Code.GDWesleyObjects2);
+for(var i = 0, k = 0, l = gdjs.Lvl2Code.GDWesleyObjects2.length;i<l;++i) {
+    if ( gdjs.Lvl2Code.GDWesleyObjects2[i].isCollidingWithPoint(0, 0) ) {
+        gdjs.Lvl2Code.condition0IsTrue_1.val = true;
+        gdjs.Lvl2Code.GDWesleyObjects2[k] = gdjs.Lvl2Code.GDWesleyObjects2[i];
+        ++k;
+    }
+}
+gdjs.Lvl2Code.GDWesleyObjects2.length = k;if( gdjs.Lvl2Code.condition0IsTrue_1.val ) {
+    gdjs.Lvl2Code.conditionTrue_1.val = true;
+    for(var j = 0, jLen = gdjs.Lvl2Code.GDWesleyObjects2.length;j<jLen;++j) {
+        if ( gdjs.Lvl2Code.GDWesleyObjects1_1final.indexOf(gdjs.Lvl2Code.GDWesleyObjects2[j]) === -1 )
+            gdjs.Lvl2Code.GDWesleyObjects1_1final.push(gdjs.Lvl2Code.GDWesleyObjects2[j]);
+    }
+}
+}
+{
+gdjs.copyArray(runtimeScene.getObjects("Wesley"), gdjs.Lvl2Code.GDWesleyObjects2);
+for(var i = 0, k = 0, l = gdjs.Lvl2Code.GDWesleyObjects2.length;i<l;++i) {
+    if ( gdjs.Lvl2Code.GDWesleyObjects2[i].isCollidingWithPoint(0, 0) ) {
+        gdjs.Lvl2Code.condition1IsTrue_1.val = true;
+        gdjs.Lvl2Code.GDWesleyObjects2[k] = gdjs.Lvl2Code.GDWesleyObjects2[i];
+        ++k;
+    }
+}
+gdjs.Lvl2Code.GDWesleyObjects2.length = k;if( gdjs.Lvl2Code.condition1IsTrue_1.val ) {
+    gdjs.Lvl2Code.conditionTrue_1.val = true;
+    for(var j = 0, jLen = gdjs.Lvl2Code.GDWesleyObjects2.length;j<jLen;++j) {
+        if ( gdjs.Lvl2Code.GDWesleyObjects1_1final.indexOf(gdjs.Lvl2Code.GDWesleyObjects2[j]) === -1 )
+            gdjs.Lvl2Code.GDWesleyObjects1_1final.push(gdjs.Lvl2Code.GDWesleyObjects2[j]);
+    }
+}
+}
+{
+gdjs.copyArray(runtimeScene.getObjects("Wesley"), gdjs.Lvl2Code.GDWesleyObjects2);
+for(var i = 0, k = 0, l = gdjs.Lvl2Code.GDWesleyObjects2.length;i<l;++i) {
+    if ( gdjs.Lvl2Code.GDWesleyObjects2[i].isCollidingWithPoint(0, 0) ) {
+        gdjs.Lvl2Code.condition2IsTrue_1.val = true;
+        gdjs.Lvl2Code.GDWesleyObjects2[k] = gdjs.Lvl2Code.GDWesleyObjects2[i];
+        ++k;
+    }
+}
+gdjs.Lvl2Code.GDWesleyObjects2.length = k;if( gdjs.Lvl2Code.condition2IsTrue_1.val ) {
+    gdjs.Lvl2Code.conditionTrue_1.val = true;
+    for(var j = 0, jLen = gdjs.Lvl2Code.GDWesleyObjects2.length;j<jLen;++j) {
+        if ( gdjs.Lvl2Code.GDWesleyObjects1_1final.indexOf(gdjs.Lvl2Code.GDWesleyObjects2[j]) === -1 )
+            gdjs.Lvl2Code.GDWesleyObjects1_1final.push(gdjs.Lvl2Code.GDWesleyObjects2[j]);
+    }
+}
+}
+{
+gdjs.copyArray(runtimeScene.getObjects("Wesley"), gdjs.Lvl2Code.GDWesleyObjects2);
+for(var i = 0, k = 0, l = gdjs.Lvl2Code.GDWesleyObjects2.length;i<l;++i) {
+    if ( gdjs.Lvl2Code.GDWesleyObjects2[i].isCollidingWithPoint(0, 0) ) {
+        gdjs.Lvl2Code.condition3IsTrue_1.val = true;
+        gdjs.Lvl2Code.GDWesleyObjects2[k] = gdjs.Lvl2Code.GDWesleyObjects2[i];
+        ++k;
+    }
+}
+gdjs.Lvl2Code.GDWesleyObjects2.length = k;if( gdjs.Lvl2Code.condition3IsTrue_1.val ) {
+    gdjs.Lvl2Code.conditionTrue_1.val = true;
+    for(var j = 0, jLen = gdjs.Lvl2Code.GDWesleyObjects2.length;j<jLen;++j) {
+        if ( gdjs.Lvl2Code.GDWesleyObjects1_1final.indexOf(gdjs.Lvl2Code.GDWesleyObjects2[j]) === -1 )
+            gdjs.Lvl2Code.GDWesleyObjects1_1final.push(gdjs.Lvl2Code.GDWesleyObjects2[j]);
+    }
+}
+}
+{
+gdjs.copyArray(runtimeScene.getObjects("Wesley"), gdjs.Lvl2Code.GDWesleyObjects2);
+for(var i = 0, k = 0, l = gdjs.Lvl2Code.GDWesleyObjects2.length;i<l;++i) {
+    if ( gdjs.Lvl2Code.GDWesleyObjects2[i].isCollidingWithPoint(0, 0) ) {
+        gdjs.Lvl2Code.condition4IsTrue_1.val = true;
+        gdjs.Lvl2Code.GDWesleyObjects2[k] = gdjs.Lvl2Code.GDWesleyObjects2[i];
+        ++k;
+    }
+}
+gdjs.Lvl2Code.GDWesleyObjects2.length = k;if( gdjs.Lvl2Code.condition4IsTrue_1.val ) {
+    gdjs.Lvl2Code.conditionTrue_1.val = true;
+    for(var j = 0, jLen = gdjs.Lvl2Code.GDWesleyObjects2.length;j<jLen;++j) {
+        if ( gdjs.Lvl2Code.GDWesleyObjects1_1final.indexOf(gdjs.Lvl2Code.GDWesleyObjects2[j]) === -1 )
+            gdjs.Lvl2Code.GDWesleyObjects1_1final.push(gdjs.Lvl2Code.GDWesleyObjects2[j]);
+    }
+}
+}
+{
+gdjs.copyArray(runtimeScene.getObjects("Wesley"), gdjs.Lvl2Code.GDWesleyObjects2);
+for(var i = 0, k = 0, l = gdjs.Lvl2Code.GDWesleyObjects2.length;i<l;++i) {
+    if ( gdjs.Lvl2Code.GDWesleyObjects2[i].isCollidingWithPoint(0, 0) ) {
+        gdjs.Lvl2Code.condition5IsTrue_1.val = true;
+        gdjs.Lvl2Code.GDWesleyObjects2[k] = gdjs.Lvl2Code.GDWesleyObjects2[i];
+        ++k;
+    }
+}
+gdjs.Lvl2Code.GDWesleyObjects2.length = k;if( gdjs.Lvl2Code.condition5IsTrue_1.val ) {
+    gdjs.Lvl2Code.conditionTrue_1.val = true;
+    for(var j = 0, jLen = gdjs.Lvl2Code.GDWesleyObjects2.length;j<jLen;++j) {
+        if ( gdjs.Lvl2Code.GDWesleyObjects1_1final.indexOf(gdjs.Lvl2Code.GDWesleyObjects2[j]) === -1 )
+            gdjs.Lvl2Code.GDWesleyObjects1_1final.push(gdjs.Lvl2Code.GDWesleyObjects2[j]);
+    }
+}
+}
+{
+gdjs.copyArray(gdjs.Lvl2Code.GDWesleyObjects1_1final, gdjs.Lvl2Code.GDWesleyObjects1);
+}
+}
+}}
+if (gdjs.Lvl2Code.condition1IsTrue_0.val) {
+{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Lvl2", false);
+}}
+
+}
+
+
+{
+
+
+{
+}
+
+}
+
+
+};
+
+gdjs.Lvl2Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Lvl2Code.GDStars_95ParticlesObjects1.length = 0;
+gdjs.Lvl2Code.GDStars_95ParticlesObjects2.length = 0;
+gdjs.Lvl2Code.GDRoomTrapsObjects1.length = 0;
+gdjs.Lvl2Code.GDRoomTrapsObjects2.length = 0;
+gdjs.Lvl2Code.GDRoomDoorsObjects1.length = 0;
+gdjs.Lvl2Code.GDRoomDoorsObjects2.length = 0;
+gdjs.Lvl2Code.GDRoomFloorObjects1.length = 0;
+gdjs.Lvl2Code.GDRoomFloorObjects2.length = 0;
+gdjs.Lvl2Code.GDRoomObjects1.length = 0;
+gdjs.Lvl2Code.GDRoomObjects2.length = 0;
+gdjs.Lvl2Code.GDWesleyObjects1.length = 0;
+gdjs.Lvl2Code.GDWesleyObjects2.length = 0;
+gdjs.Lvl2Code.GDGunObjects1.length = 0;
+gdjs.Lvl2Code.GDGunObjects2.length = 0;
+gdjs.Lvl2Code.GDImpObjects1.length = 0;
+gdjs.Lvl2Code.GDImpObjects2.length = 0;
+gdjs.Lvl2Code.GDSpiderObjects1.length = 0;
+gdjs.Lvl2Code.GDSpiderObjects2.length = 0;
+gdjs.Lvl2Code.GDGhostObjects1.length = 0;
+gdjs.Lvl2Code.GDGhostObjects2.length = 0;
+gdjs.Lvl2Code.GDGhostOrbObjects1.length = 0;
+gdjs.Lvl2Code.GDGhostOrbObjects2.length = 0;
+gdjs.Lvl2Code.GDBulletObjects1.length = 0;
+gdjs.Lvl2Code.GDBulletObjects2.length = 0;
+gdjs.Lvl2Code.GDUpgradeTextObjects1.length = 0;
+gdjs.Lvl2Code.GDUpgradeTextObjects2.length = 0;
+gdjs.Lvl2Code.GDEnemyDamageTextObjects1.length = 0;
+gdjs.Lvl2Code.GDEnemyDamageTextObjects2.length = 0;
+gdjs.Lvl2Code.GDParticle_95RecoilDustObjects1.length = 0;
+gdjs.Lvl2Code.GDParticle_95RecoilDustObjects2.length = 0;
+gdjs.Lvl2Code.GDParticle_95DashObjects1.length = 0;
+gdjs.Lvl2Code.GDParticle_95DashObjects2.length = 0;
+gdjs.Lvl2Code.GDParticle_95DeathObjects1.length = 0;
+gdjs.Lvl2Code.GDParticle_95DeathObjects2.length = 0;
+gdjs.Lvl2Code.GDHealthBarBorderObjects1.length = 0;
+gdjs.Lvl2Code.GDHealthBarBorderObjects2.length = 0;
+gdjs.Lvl2Code.GDHealthBarObjects1.length = 0;
+gdjs.Lvl2Code.GDHealthBarObjects2.length = 0;
+gdjs.Lvl2Code.GDDebugObjects1.length = 0;
+gdjs.Lvl2Code.GDDebugObjects2.length = 0;
+gdjs.Lvl2Code.GDUpgradeIconsObjects1.length = 0;
+gdjs.Lvl2Code.GDUpgradeIconsObjects2.length = 0;
+gdjs.Lvl2Code.GDUpgradesObjects1.length = 0;
+gdjs.Lvl2Code.GDUpgradesObjects2.length = 0;
+gdjs.Lvl2Code.GDTotalPointsCountObjects1.length = 0;
+gdjs.Lvl2Code.GDTotalPointsCountObjects2.length = 0;
+gdjs.Lvl2Code.GDTotalPointsObjects1.length = 0;
+gdjs.Lvl2Code.GDTotalPointsObjects2.length = 0;
+gdjs.Lvl2Code.GDDangerLevelCountObjects1.length = 0;
+gdjs.Lvl2Code.GDDangerLevelCountObjects2.length = 0;
+gdjs.Lvl2Code.GDDangerLevelObjects1.length = 0;
+gdjs.Lvl2Code.GDDangerLevelObjects2.length = 0;
+gdjs.Lvl2Code.GDHealthTextObjects1.length = 0;
+gdjs.Lvl2Code.GDHealthTextObjects2.length = 0;
+gdjs.Lvl2Code.GDPick_95UpsObjects1.length = 0;
+gdjs.Lvl2Code.GDPick_95UpsObjects2.length = 0;
+gdjs.Lvl2Code.GDReset_95TimerObjects1.length = 0;
+gdjs.Lvl2Code.GDReset_95TimerObjects2.length = 0;
+gdjs.Lvl2Code.GDLeaderboard_95SubmitObjects1.length = 0;
+gdjs.Lvl2Code.GDLeaderboard_95SubmitObjects2.length = 0;
+gdjs.Lvl2Code.GDReset_95LeaderboardObjects1.length = 0;
+gdjs.Lvl2Code.GDReset_95LeaderboardObjects2.length = 0;
+gdjs.Lvl2Code.GDReset_95ButtonObjects1.length = 0;
+gdjs.Lvl2Code.GDReset_95ButtonObjects2.length = 0;
+gdjs.Lvl2Code.GDDarkeningObjects1.length = 0;
+gdjs.Lvl2Code.GDDarkeningObjects2.length = 0;
+gdjs.Lvl2Code.GDLeaderboardName_95InputObjects1.length = 0;
+gdjs.Lvl2Code.GDLeaderboardName_95InputObjects2.length = 0;
+gdjs.Lvl2Code.GDPause_95TextObjects1.length = 0;
+gdjs.Lvl2Code.GDPause_95TextObjects2.length = 0;
+gdjs.Lvl2Code.GDUpgrade_95TextObjects1.length = 0;
+gdjs.Lvl2Code.GDUpgrade_95TextObjects2.length = 0;
+
+gdjs.Lvl2Code.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['Lvl2Code'] = gdjs.Lvl2Code;
